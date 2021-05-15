fdc15456a434e593245d7b0ef6d06494e917d991
Christopher <visrule@protonmail.com>
2021-05-15 17:16:30 -0500

    Initial Commit

---
+++ CardView/CardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewApp.swift
+//  CardView
+//
+//  Created by Christopher Hicks on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
